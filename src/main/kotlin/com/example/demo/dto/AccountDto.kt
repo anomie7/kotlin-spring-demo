51e61da17685e67d05ac90d75076a41f021ce7c8
Justin <anomie7777@gmail.com>
2020-03-08 21:39:58 +0900

    kotlin default value를 기본 생성자의 모든 속성에 할당함으로써 인자없는 생성자를 정의할 필요 없음

--- src/main/kotlin/com/example/demo/dto/AccountDto.kt
+++ src/main/kotlin/com/example/demo/dto/AccountDto.kt
@@ -3,9 +3,7 @@ package com.example.demo.dto
 import com.example.demo.entity.AccountRole
 import java.time.LocalDateTime
 
-data class RegistrableAccountDto(var email: String = "", var password: String = ""){
-    constructor() : this("", "")
-}
+data class RegistrableAccountDto(var email: String = "", var password: String = "")
 
 data class ReadableAccountDto(var id : Long?, var email: String, var roles: MutableSet<AccountRole>,var createDt: LocalDateTime)
 
